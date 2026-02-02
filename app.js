a264ea09b34b9016dded65a2b177731cb6fa7d2d
djsy104 <148027370+djsy104@users.noreply.github.com>
2026-02-01 18:05:22 -0800

    Finished week10 assignments

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ require("express-async-errors");
 const express = require("express");
 const app = express();
 
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 //Connect DB
 const connectDB = require("./db/connect");
 const authenticatedUser = require("./middleware/authentication");
@@ -16,6 +21,16 @@ const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Max of 100 requests per windowMs
+  }),
+);
 // extra packages
 
 // routes
